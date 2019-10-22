7702fa3aa19fd156be00bffd25606fad7e09a654
leonszs <47385872+leonszs@users.noreply.github.com>
2019-10-22 10:06:48 -0300

    Add files via upload

---
+++ mem dinamica/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{
+    int legajo;
+    char sexo;
+    float altura;
+}ePersona;
+
+ePersona * new_Persona();
+
+
+
+int main()
+{
+   ePersona* puntero = new_Persona();
+   printf("%d---%c-----%f", puntero)
+    return 0;
+}
+
+ePersona* new_Persona();
+{
+
+    ePersona unaPersona = (1000, 'f' , 75);
+    ePersona*puntero;
+    puntero = &unaPersona
+
+    return puntero;
+}
+
+
+un puntero es una opcion para guardar una variable
+
+
+QUE ES UN SIZOOF
+
+
+stack
+MEMORIA ESTATICA
+
+MEMORIA DINAMICA(HEAD/monton)
