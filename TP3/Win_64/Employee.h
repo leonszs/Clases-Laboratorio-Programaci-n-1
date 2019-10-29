e808af77fbdeb81c741462b5e9078bf47bb01c47
leonszs <47385872+leonszs@users.noreply.github.com>
2019-10-29 10:32:51 -0300

    Add files via upload

---
+++ TP3/Win_64/Employee.h
@@ -0,0 +1,51 @@
+#ifndef employee_H_INCLUDED
+#define employee_H_INCLUDED
+typedef struct
+{
+    int id;
+    char nombre[128];
+    int horasTrabajadas;
+    int sueldo;
+}Employee;
+
+Employee* employee_new();
+Employee* employee_newParametros(char* idStr,char* nombreStr,char* horasTrabajadasStr);
+void employee_delete();
+
+int employee_setId(Employee* this,int id);
+int employee_getId(Employee* this,int* id);
+
+int employee_setNombre(Employee* this,char* nombre);
+int employee_getNombre(Employee* this,char* nombre);
+
+int employee_setHorasTrabajadas(Employee* this,int horasTrabajadas);
+int employee_getHorasTrabajadas(Employee* this,int* horasTrabajadas);
+
+int employee_setSueldo(Employee* this,int sueldo);
+int employee_getSueldo(Employee* this,int* sueldo);
+
+int compararPorLegajo(void* e1 , void* e2);
+{   float sueldo1;
+    float sueldo2;
+    int comparar;
+
+    Employee* empelado1 = (Employee*)e1;
+
+    Employee* empleado2 = (Employee*)e2;
+
+    if (employee_getId(empleado1,&id1) > employee_getId(empleado2,&id2))
+    {
+
+        comparar = 1;
+
+    }
+    else
+    {
+       if (employee_getId(empleado1,&id1) == employee_getId(empleado2,&id2))
+       {
+           comparar = 0;
+       }
+    }
+    return comparar;
+}
+#endif // employee_H_INCLUDED
---
+++ TP3/Win_64/main.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "LinkedList.h"
+#include "Controller.h"
+#include "Employee.h"
+
+/****************************************************
+    Menu:
+     1. Cargar los datos de los empleados desde el archivo data.csv (modo texto).
+     2. Cargar los datos de los empleados desde el archivo data.csv (modo binario).
+     3. Alta de empleado
+     4. Modificar datos de empleado
+     5. Baja de empleado
+     6. Listar empleados
+     7. Ordenar empleados
+     8. Guardar los datos de los empleados en el archivo data.csv (modo texto).
+     9. Guardar los datos de los empleados en el archivo data.csv (modo binario).
+    10. Salir
+*****************************************************/
+
+
+int main()
+{
+    il_add (listaempleads,e1);
+il_add(listaempleads,e2);
+il_add (listaempleads,e3);
+il_add (listaempleads,e4);
+
+employe*aux
+agreguè 3 empleados.
+
+
+mostrar elementos:
+int = x;
+int len;
+len = il_len (listaempleads);    para saber cuantos hay
+for (x=0; x<len;i++)
+{
+
+aux = (employe*) il_get(listaempleads,x);                   ()casteo //
+
+
+}
+
+il_set  (listaempleads,2,e4)              agrega un elemento sobre otro
+
+
+
+
+    int option = 0;
+    LinkedList* listaEmpleados = ll_newLinkedList();
+    do{
+        switch(option)
+        {
+            case 1:
+                controller_loadFromText("data.csv",listaEmpleados);
+                break;
+        }
+    }while(option != 10);
+    return 0;
+}
